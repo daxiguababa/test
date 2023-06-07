3f2adde0cf913020191268892d31d9344304a0ce
龙伟 <18161364646@18161364646.com>
2023-06-07 11:15:28 +0800

    hello world

--- main.go
+++ main.go
@@ -9,11 +9,8 @@ import (
 func main() {
 	r := gin.Default()
 
-	//r.LoadHTMLGlob("html/views/admin/*")
 	r.StaticFS("/", http.Dir("./html/static/page"))
 
-	//定时任务
-	//task.Start()
 	if err := r.Run(":8181"); err != nil {
 		log.Fatalln("启动服务失败：", err.Error())
 	}
